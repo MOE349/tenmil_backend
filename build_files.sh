02072798b40ef37ccb2b5ad498b69a8749407138
FerasAlfrihDev <feras.alfrih.dev@gmail.com>
2025-04-25 20:25:31 +0300

    init

--- build_files.sh
+++ build_files.sh
@@ -4,8 +4,8 @@ echo "installing requirements"
 python3 -m pip install -r requirements.txt
 
 echo "creating database"
-python3 manage.py makemigrations
-python3 manage.py migrate
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "collect statics"
 python3 manage.py collectstatic --noinput
