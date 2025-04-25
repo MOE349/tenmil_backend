54edef3489566d8ca24d5a419f9a5d1f75b6fef6
FerasAlfrihDev <feras.alfrih.dev@gmail.com>
2025-04-25 22:29:51 +0300

    init

--- configurations/system_start_checks.py
+++ configurations/system_start_checks.py
@@ -6,21 +6,23 @@ from core.models import Domain, Tenant, WorkOrderStatusControls
 
 def public_tenant_check():
     try:
-        Tenant.objects.filter(schema_name='public')
-    except:
-        # create your public tenant
-        tenant = Tenant(schema_name='public',
-                        name='Tenmil Inc.',
-                        paid_until='2100-12-05',
-                        on_trial=False)
-        tenant.save()
+        tenant = Tenant.objects.filter(schema_name='public')
+        if not tenant.exists():
+            # create your public tenant
+            tenant = Tenant(schema_name='public',
+                            name='Tenmil Inc.',
+                            paid_until='2100-12-05',
+                            on_trial=False)
+            tenant.save()
 
-        # Add one or more domains for the tenant
-        domain = Domain()
-        domain.domain = f"{settings.BASE_DOMAIN}" # don't add your port or www here! on a local server you'll want to use localhost here
-        domain.tenant = tenant
-        domain.is_primary = True
-        domain.save()
+            # Add one or more domains for the tenant
+            domain = Domain()
+            domain.domain = f"{settings.BASE_DOMAIN}" # don't add your port or www here! on a local server you'll want to use localhost here
+            domain.tenant = tenant
+            domain.is_primary = True
+            domain.save()
+    except:
+        pass
 
 def work_order_status_actions_check():
     """
