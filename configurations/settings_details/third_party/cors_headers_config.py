0ce1147a51da1c86eed7d987d385aa71fb05081f
FerasAlfrihDev <feras.alfrih.dev@gmail.com>
2025-04-25 21:34:56 +0300

    init

--- configurations/settings_details/third_party/cors_headers_config.py
+++ configurations/settings_details/third_party/cors_headers_config.py
@@ -1,8 +1,12 @@
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3039",
-# ]
-# CSRF_TRUSTED_ORIGINS = [
-#     "http://localhost:3039",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3039",
+    "https://alfrih.com",
+    "https://www.alfrih.com"
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3039",
+    "https://alfrih.com",
+    "https://www.alfrih.com"
+]
