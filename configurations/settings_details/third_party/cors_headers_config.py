6ff217a248034c67a80e2acb38da13e5cbbcf102
FerasAlfrihDev <feras.alfrih.dev@gmail.com>
2025-04-25 20:47:31 +0300

    init

--- configurations/settings_details/third_party/cors_headers_config.py
+++ configurations/settings_details/third_party/cors_headers_config.py
@@ -1,7 +1,8 @@
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3039",
 # ]
 # CSRF_TRUSTED_ORIGINS = [
 #     "http://localhost:3039",
 # ]
-CORS_ORIGIN_ALLOW_ALL = True
