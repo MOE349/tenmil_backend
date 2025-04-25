d1683fc8e407fbed7172ee022ef3f33c50500a6c
FerasAlfrihDev <feras.alfrih.dev@gmail.com>
2025-04-25 20:42:35 +0300

    init

--- configurations/urls.py
+++ configurations/urls.py
@@ -59,4 +59,4 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# system_start_checks()
+system_start_checks()
