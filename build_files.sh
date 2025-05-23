ae5dbff2b10a2e0d21d233c6248249e8905a4015
FerasAlfrihDev <feras.alfrih.dev@gmail.com>
2025-05-24 01:04:16 +0300

    dashboard

--- build_files.sh
+++ build_files.sh
@@ -6,8 +6,11 @@ python3 -m pip install -r requirements.txt
 echo "creating database makemigrations"
 python3 manage.py makemigrations --noinput
 
+echo "creating database migrate tenants and domains"
+python3 manage.py migrate core --noinput
+
 echo "creating database migrate"
-python3 manage.py migrate_schemas --noinput
+python3 manage.py migrate --noinput
 
 echo "collect statics"
 python3 manage.py collectstatic --noinput
