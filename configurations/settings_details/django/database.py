b4dabe218729732d778bf423345681571507ba10
FerasAlfrihDev <feras.alfrih.dev@gmail.com>
2025-07-05 01:26:09 +0300

    db

--- configurations/settings_details/django/database.py
+++ configurations/settings_details/django/database.py
@@ -1,17 +1,19 @@
 from configurations.settings_details.env import env
 from django.conf import settings
+import dj_database_url
 
 enviroment = "STAGE_" if settings.DEBUG else "PROD_"
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': env(f"{enviroment}DATABASE_ENGINE"),
-        'NAME': env(f"{enviroment}DATABASE_NAME"),
-        'USER': env(f"{enviroment}DATABASE_USER"),
-        'PASSWORD': env(f"{enviroment}DATABASE_PASSWORD"),
-        'HOST': env(f"{enviroment}DATABASE_HOST"),
-        'PORT': env(f"{enviroment}DATABASE_PORT"),
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    # 'default': {
+    #     'ENGINE': env(f"{enviroment}DATABASE_ENGINE"),
+    #     'NAME': env(f"{enviroment}DATABASE_NAME"),
+    #     'USER': env(f"{enviroment}DATABASE_USER"),
+    #     'PASSWORD': env(f"{enviroment}DATABASE_PASSWORD"),
+    #     'HOST': env(f"{enviroment}DATABASE_HOST"),
+    #     'PORT': env(f"{enviroment}DATABASE_PORT"),
+    # }
 }
 # DATABASES = {
 #     'default': {
