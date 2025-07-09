98e3e09e1debfbd3707f678ccdb164a3c317ab78
FerasAlfrihDev <feras.alfrih.dev@gmail.com>
2025-07-09 22:21:59 +0300

    gunicorn conf update

--- configurations/gunicorn.conf.py
+++ configurations/gunicorn.conf.py
@@ -1,18 +1,25 @@
 # gunicorn.conf.py
 
 import multiprocessing
-
-from configurations.system_start_checks import system_start_checks
+import os
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "sync"
 timeout = 120
-preload_app = True  # IMPORTANT: enables `when_ready` hook to work
+preload_app = True  # enables when_ready()
 
 accesslog = "-"
 errorlog = "-"
 
 def when_ready(server):
-    print("[Gunicorn] Running system_start_checks()...")
+    import django
+    from django.conf import settings
+
+    print("[Gunicorn] Initializing Django...")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")  # adjust this!
+    django.setup()
+
+    print("[Gunicorn] Running system_start_checks()")
+    from configurations.system_start_checks import system_start_checks
     system_start_checks()
