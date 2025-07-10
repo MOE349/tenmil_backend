da0b23b02075b99024dd47c8ef2ff367f913c599
FerasAlfrihDev <feras.alfrih.dev@gmail.com>
2025-07-10 19:52:20 +0300

    threads

--- configurations/gunicorn.conf.py
+++ configurations/gunicorn.conf.py
@@ -5,6 +5,7 @@ import os
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
+threads = 2 * workers
 worker_class = "sync"
 timeout = 120
 preload_app = True  # enables when_ready()
