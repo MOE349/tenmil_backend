bd058f33ce7a70eb4b6d3d048074028c47dcf0af
FerasAlfrihDev <feras.alfrih.dev@gmail.com>
2025-07-17 21:34:33 +0300

    cmpltn nt

--- work_orders/apps.py
+++ work_orders/apps.py
@@ -4,13 +4,13 @@ from django.db.models.base import post_save
 from django.db.models.signals import post_migrate
 from django.db import connection
 
-from work_orders.models import WorkOrder
 
 class WorkOrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'work_orders'
 
     def ready(self):
+        from work_orders.models import WorkOrder
         from .signals import create_default_status_names, create_work_order_completion_note  # noqa
         post_migrate.connect(create_default_status_names, sender=self)
-        post_save.connect(create_work_order_completion_note, sender=WorkOrder)
+        post_save.connect(create_work_order_completion_note, sender=WorkOrder, dispatch_uid='create_work_order_completion_note')
